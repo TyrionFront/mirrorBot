cf33ecfdfb75de94ca3218f71712241548f86560
TyrionFront <GingerTormund@protonmail.com>
2021-05-14 08:19:56 +0300

    change configs

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import botApp from './app';
 import 'regenerator-runtime';
 
 try {
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8001;
   const url = process.env.BOT_URL_HEROKU || process.env.BOT_URL;
   botApp(url).listen(port, () => {
     console.log(`App server is now listening on ${port} !`);
